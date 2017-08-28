029732aed092ec510dba64786dd5c102a6a9c8c7
Lucas Simon <lucas.oscar.simon@gmail.com>
2017-08-28 08:21:25 -0300

    Primer commit, falta corregir ya que aun no funciona

--- MiniMaxSum/Program.cs
+++ MiniMaxSum/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniMaxSum
+class Solution
 {
-    class Program
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        string[] result = new string[arr.Length];
+        var strRes = string.Empty;
+        
+        for (int i = 0; i < arr.Length; i++)
         {
+            result[i] = arr.Where(val => val != arr[i]).Sum().ToString();
         }
+        strRes = result.Min() + " " + result.Max();
+        Console.WriteLine(strRes);
+        Console.ReadLine();
     }
 }
