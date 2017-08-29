b4fb46f5c31e7f6519e6654e283770c3ea73ef0e
Lucas Simon <lucas.oscar.simon@gmail.com>
2017-08-29 20:33:16 -0300

    Almost solved. There's some cases where the test still fail

--- MiniMaxSum/Program.cs
+++ MiniMaxSum/Program.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-class Solution
+
+namespace MinMaxSum
 {
-    static void Main(String[] args)
+    class Program
     {
-        string[] arr_temp = Console.ReadLine().Split(' ');
-        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
-        string[] result = new string[arr.Length];
-        var strRes = string.Empty;
-        
-        for (int i = 0; i < arr.Length; i++)
+        static void Main(String[] args)
         {
-            result[i] = arr.Where(val => val != arr[i]).Sum().ToString();
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            var arr = Array.ConvertAll(arr_temp, Int64.Parse);
+            var result = new Int64[arr.Length];
+            var strRes = string.Empty;
+            var length = arr.Length - 1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i <= length)
+                {
+                    result[i] = arr.Where(e => e != arr[i]).ToArray().Sum() != 0 ?
+                        arr.Where(e => e != arr[i]).ToArray().Sum() :
+                        arr.Skip(1).Where(e => e == arr[i]).ToArray().Sum();
+                }
+            }
+
+            strRes = result.Min() + " " + result.Max();
+            Console.WriteLine(strRes);
+            Console.ReadLine();
         }
-        strRes = result.Min() + " " + result.Max();
-        Console.WriteLine(strRes);
-        Console.ReadLine();
     }
 }
